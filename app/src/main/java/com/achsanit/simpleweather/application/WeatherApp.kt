900a3400a0d95d834d25509504e6467b842fb9e7
Achsani Taqwim <achsanit87@gmail.com>
2023-11-28 16:14:19 +0800

    fix missing current location

--- app/src/main/java/com/achsanit/simpleweather/application/WeatherApp.kt
+++ app/src/main/java/com/achsanit/simpleweather/application/WeatherApp.kt
@@ -1,7 +1,14 @@
 package com.achsanit.simpleweather.application
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class WeatherApp: Application()
+class WeatherApp: Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+    }
+}
